507503bd88027d14f2537336a2e71a51b0f90ef0
duybv1801 <duybv1801@gmail.com>
2023-07-13 14:55:33 +0700

    update postMeta 13/7/2023

--- app/Models/PostMeta.php
+++ app/Models/PostMeta.php
@@ -13,26 +13,6 @@ class PostMeta extends Model
 {
     use HasFactory;
     protected $fillable = ['key', 'value'];
-    protected $dataTypes = ['boolean', 'integer', 'double', 'float', 'string', 'NULL'];
-
-
-
-    public function getValueAttribute($value)
-    {
-        $type = $this->type ?: 'null';
-
-        switch ($type) {
-            case 'array':
-                return json_decode($value, true);
-            case 'file':
-                return $value;
-        }
-        if (in_array($type, $this->dataTypes)) {
-            settype($value, $type);
-        }
-
-        return $value;
-    }
 
     public function post()
     {
